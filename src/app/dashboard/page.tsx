9583ea24fa0d8f719efcb0fa2ba62ed43d71f2af
frntprog <frntprog@gmail.com>
2023-07-17 23:58:07 +0300

    started new module

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,13 @@
 import Button from "@/components/ui/Button";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import { FC } from "react";
 
 interface pageProps {}
 
-const page: FC<pageProps> = ({}) => {
+const page = async ({}) => {
+  const session = await getServerSession(authOptions);
+
   return <Button>hello</Button>;
 };
 
